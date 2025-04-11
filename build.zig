ce4c446199560c9b0b7bfc7dd1c711418adc646b
lalilu <1248393538@qq.com>
2025-04-12 00:47:28 +0800

    build: 修改目标平台设置
    - 注释掉原有的标准目标选项- 新增特定于 Linux 的目标查询
    - 使用 resolveTargetQuery 解析目标平台

--- build.zig
+++ build.zig
@@ -8,7 +8,9 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    // const target = b.standardTargetOptions(.{});
+    const target_query = std.Target.Query{ .os_tag = .linux };
+    const target = b.resolveTargetQuery(target_query);
 
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
